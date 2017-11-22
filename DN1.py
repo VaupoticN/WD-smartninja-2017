c6573c87d7afda450dee11c7700b4df0a9184edd
VaupoticN <33127469+VaupoticN@users.noreply.github.com>
2017-11-22 17:16:40 +0100

    Add files via upload

--- DN1.py
+++ DN1.py
@@ -1,17 +1,15 @@
 print "Hi, hello! " \
       "Let me introduce myself - my name is KMtoML and I do a splendid job of converting from kilometers to miles."
 
-km = float( raw_input("Please, enter kilometers here:"))
-
-print "This equals to", km*0.621, "miles"
 
 while True:
+      km = float(raw_input("Please, enter kilometers here:"))
+      print "This equals to", km * 0.621, "miles"
       another = raw_input("Do you wish to do another conversion? Please answer with yes or no.")
 
       if another == "no":
             print "Okay! :) Have a nice day!"
             break
 
-      if another == "yes":
-            km2 = float( raw_input("Please, enter kilometers here:"))
-            print "This equals to", km2*0.621, "miles"
+      if another != "no" and another != "yes":
+            print "Please answer with yes or no!"
